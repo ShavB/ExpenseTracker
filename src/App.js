c38ce56204d136e6e52be3e092f70c3ea2402792
shavb <bishtshyam135@gmail.com>
2022-06-15 22:11:34 +0530

    Base of project added

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+
+// "https://randomuser.me/api/?results=20"  
+
+
+export default function App() {
+  
+  return (
+    <div className="App">
+
+      <h1>Hello world</h1>
+    </div>
+  );
+}
+
